528ca5abeb9e687c68e1be15e1efd79bfd5b9e9a
Steven Kane <steven.kane@muenchen.de>
2025-10-28 14:13:02 +0100

    Modify UserFormsReferenz, so the 'WriteOnly' wont be shown

--- eakte-eai/src/main/java/de/muenchen/dms/common/model/UserFormsReferenz.java
+++ eakte-eai/src/main/java/de/muenchen/dms/common/model/UserFormsReferenz.java
@@ -1,6 +1,8 @@
 package de.muenchen.dms.common.model;
 
 import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.EqualsAndHashCode;
@@ -17,6 +19,9 @@ import lombok.ToString;
 @EqualsAndHashCode(callSuper = false)
 @ToString
 public class UserFormsReferenz {
+  @JsonProperty("lHMBAI_15_1700_ufreference")
   private String LHMBAI_15_1700_ufreference;
+
+  @JsonProperty("lHMBAI_15_1700_ufvalue")
   private List<String> LHMBAI_15_1700_ufvalue;
 }
